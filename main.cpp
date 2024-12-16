92699e04d7e44eac1b38ce46b696c0794338ab3a
Jordan Viramontes <jvira007@ucr.edu>
2024-12-16 10:03:12 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+CS Profile Manager Profile Script - Jordan Viramontes
+
+The purpose of this script is to:
+    1) Parse through an already existing profile.dat file and copy its contents, and then
+    2) Create an entirely new profile.dat with user inputted variables
+
+Written by Jordan Viramontes with proofreading and verification by Periwinkle
+*/
+
+#include <iostream>
+#include <fstream>
+
